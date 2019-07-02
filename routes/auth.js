dc498b0ccc2a525573a82eed2c0c1624ae41cef4
angel <angelsol299@hotmail.com>
2019-07-02 19:52:43 +0200

    auth middleware and protecting routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
