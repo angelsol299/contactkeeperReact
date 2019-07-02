ee9548cf8207e88c5d5631974ef927e910e4be68
angel <angelsol299@hotmail.com>
2019-07-02 20:48:22 +0200

    contact model and contacts route

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,14 @@ const User = require('../models/User');
 // @route GET api/auth
 // @desc Get logged in a user
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get logged in a user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route POST api/auth
