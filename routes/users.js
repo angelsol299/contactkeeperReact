80e809e07cf1948828c84622f14d6aac69524984
angel <angelsol299@hotmail.com>
2019-07-02 17:05:15 +0200

    user model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
 
@@ -23,7 +23,11 @@ router.post(
     })
   ],
   (req, res) => {
-    res.send(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed');
   }
 );
 
