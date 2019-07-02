44788315f3f307d88eea1ada0342fcfafaac1fbf
angel <angelsol299@hotmail.com>
2019-07-02 21:15:56 +0200

    add contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -34,10 +34,25 @@ router.post(
         .isEmpty()
     ]
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
   }
 );
