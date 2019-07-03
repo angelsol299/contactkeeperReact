1120c4faf2d7fa8cf8b479041db69e531bfe1aca
angel <angelsol299@hotmail.com>
2019-07-03 18:10:10 +0200

    contact form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -60,7 +60,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact
       }}
     >
       {props.children}
