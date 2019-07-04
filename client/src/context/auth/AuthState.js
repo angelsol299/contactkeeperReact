7ce9ca9bc3be6de261ec7282bcfd15954c9f99cc
angel <angelsol299@hotmail.com>
2019-07-04 21:39:18 +0200

    load user and set the token

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -25,7 +25,15 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load user
-  const loadUser = () => console.log('loaduser');
+  const loadUser = async () => {
+    // @todo - load token into global headers
+    try {
+      const res = await axios.get('/api/auth');
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   //Register user
   const register = async formData => {
