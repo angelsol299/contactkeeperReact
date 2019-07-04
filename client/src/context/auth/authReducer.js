426c09a5672d50a17201cc1db8154cf3d9d3b7f5
angel <angelsol299@hotmail.com>
2019-07-04 22:56:06 +0200

    adding a private route

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -36,7 +36,7 @@ export default (state, action) => {
         ...state,
         token: null,
         isAuthenticated: false,
-        laoding: false,
+        loading: false,
         user: null,
         error: action.payload
       };
