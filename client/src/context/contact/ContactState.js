6562d0ae6d2ba4098ba5a0735a27132b9f3857fe
angel <angelsol299@hotmail.com>
2019-07-05 01:43:17 +0200

    update contacts on the DB

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -64,8 +64,33 @@ const ContactState = props => {
   //Delete contact
   const deleteContact = async id => {
     try {
-      await axios.delete(`/api/contacts/{id}`);
+      await axios.delete(`/api/contacts/${id}`);
       dispatch({ type: DELETE_CONTACT, payload: id });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+
+  //Update contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
     } catch (err) {
       dispatch({
         type: CONTACT_ERROR,
@@ -87,11 +112,6 @@ const ContactState = props => {
   //Clear CurrentContact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  //Update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter contacts
